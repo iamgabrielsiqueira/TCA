26663b13345e4de0756c0c09baad2b5f39287ac1
Julia Maria Bazanella <juliamariabazanella32@gmail.com>
2018-10-10 11:49:35 -0300

    Tela inicial v1

---
+++ src/controller/ControllerMainWindow.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class ControllerMainWindow {
+
+    @FXML
+    public void carregarHospedes() {
+        System.out.printf("Carregando...");
+    }
+
+}

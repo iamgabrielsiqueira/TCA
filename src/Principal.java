3e3fe3043d2dbccc8a722136e35c1d2fb7473872
Gabriel Siqueira <gabreezus@gmail.com>
2018-11-13 08:58:01 -0200

    Organizando

--- src/Principal.java
+++ src/Principal.java
@@ -9,7 +9,7 @@ public class Principal extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/janelaPrincipal.fxml"));
-        primaryStage.setTitle("System");
+        primaryStage.setTitle("Sistema de Gerenciamento de Hotéis");
         primaryStage.setScene(new Scene(root, 800, 600));
         //primaryStage.getScene().getStylesheets().add("skin.css");
         primaryStage.show();
